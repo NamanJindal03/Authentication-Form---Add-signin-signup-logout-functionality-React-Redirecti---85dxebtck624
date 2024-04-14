ca99d7b6ebe60be64d2424925f0ea53b57a83d37
Naman Jindal <namanjindal113@gmail.com>
2024-04-14 22:12:52 +0530

    done

--- pages/login.js
+++ pages/login.js
@@ -1,25 +1,40 @@
 'use client';
 import React, { useState } from 'react';
 import Link from 'next/link';
+import {useRouter} from 'next/navigation'
 
 const Login = () => {
+  const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(false);
-  const [isLogIn, setIsLogIn] = useState(false);
+  // const [isLogIn, setIsLogIn] = useState(false);
 
+  function handleLogin(e){
+    e.preventDefault();
+    if(email.length <= 0 || password.length <= 0 ){
+      setError(true);
+      return;
+    }
+    const registeredUser = JSON.parse(localStorage.getItem('user'));
+    if(registeredUser && registeredUser.email === email && registeredUser.password === password){
+      // setIsLogIn(true);
+      localStorage.setItem('isLoggedIn', JSON.stringify(true));
+      router.push('/store')
+    }
+  }
   return (
     <div className='login'>
       <h2>Login</h2>
-      <form>
-        <p className='error-para'>"Email or password is invalid"</p>
+      <form onSubmit={handleLogin}>
+        {error ? <p className='error-para'>"Email or password is invalid"</p> : null}
         <div className='email-div'>
           <label htmlFor='email'>Email: </label>
-          <input type='email' id='email' />
+          <input type='email' id='email' value={email} onChange={(e)=> setEmail(e.target.value)}/>
         </div>
         <div className='password-div'>
           <label htmlFor='password'>Password: </label>
-          <input type='password' id='password' />
+          <input type='password' id='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
         </div>
         <button type='submit' className='login-btn'>
           Log In
