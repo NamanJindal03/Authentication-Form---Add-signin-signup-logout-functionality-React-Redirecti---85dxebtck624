380a966b276c0bb7e8d0c176d5dd8eba03b8b950
Naman Jindal <namanjindal113@gmail.com>
2024-04-14 22:35:32 +0530

    done

--- pages/store.js
+++ pages/store.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {useRouter} from 'next/navigation'
 
 function Store() {
+  const router = useRouter();
+
+  useEffect(()=>{
+    const isLoggedIn = JSON.parse(localStorage.getItem('isLoggedIn'));
+    if(!isLoggedIn){
+      router.push('/login');
+    }
+  },[])
+
+  function handleLogout(){
+    localStorage.setItem('isLoggedIn', JSON.stringify(false))
+    router.push('/login');
+  }
+
   return (
     <div className='store'>
       <h2>Welcome to the store!</h2>
-      <button className='logout-btn'>Logout</button>
+      <button className='logout-btn' onClick={handleLogout}>Logout</button>
     </div>
   );
 }
